d1a416693b7cd938d0bbf0a5d94ab56ec7dbf87f
Patrick <patrickwiechert@yahoo.com>
2016-05-09 15:29:37 -0500

    updated player controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,22 +1,50 @@
 class PlayersController < ApplicationController
-  def create
-  end
-
-  def update
-  end
-
   def index
+    @players=Player.new
   end
 
-  def show
-  end
-
-  def view
-  end
-
-  def destroy
+  def edit
+    @player = Player.find(params[:id])
   end
 
   def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to :players, notice: "Success"
+    else
+      flash.now[:alert] = "Couldn't save"
+      render :new
+  end
+
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  def update
+    @player = Player.find(params[:id])
+    if @player.update_attributes(player_params)
+      redirect_to player_path(@player), notice: "Successfully Updated!!!!"
+    else
+      flash.now[:alert]="Couldn't update."
+      render :edit
+  end
+
+  def view
+    @player = Player.find(params[:id])
+  end
+
+  def destroy
+    @player = Player.find(params[:id])
+    @player.destroy
+    redirect_to :players, notice: "Successfully deleted!!!"
+  end
+
+  private
+  def player_params
+        params.require(:player).permit(:name, :email)
   end
 end
