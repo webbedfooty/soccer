cf990757a0ceda02a5e668cb1c1ef930978f1148
Patrick <patrickwiechert@yahoo.com>
2016-05-09 15:15:56 -0500

    added controller

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,22 @@
+class PlayersController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def view
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+
+  get 'players/create'
+
+  get 'players/update'
+
+  get 'players/index'
+
+  get 'players/show'
+
+  get 'players/view'
+
+  get 'players/destroy'
+
+  get 'players/new'
+
+resources :players
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
