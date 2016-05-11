2f4b570ae7dc149befefd92ec3d9c59c6acde922
Patrick <patrickwiechert@yahoo.com>
2016-05-11 15:22:58 -0500

    fixed tests for teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,22 +1,48 @@
 class TeamsController < ApplicationController
-  def create
-  end
-
-  def destroy
-  end
-
-  def edit
-  end
-
   def index
+    @teams = Team.all
   end
 
   def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to :teams, notice: "Success"
+    else
+      flash.now[:alert] = "Couldn't save"
+      render :new
+    end
   end
 
   def show
+    @team = Team.find(params[:id])
   end
 
   def update
+    @team = Team.find(params[:id])
+    if @team.update_attributes(team_params)
+      redirect_to team_path(@team), notice: "Successfully Updated!!!!"
+    else
+      flash.now[:alert]="Couldn't update."
+      render :edit
+    end
+  end
+
+  def edit
+    @team = Team.find(params[:id])
+  end
+
+  def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+    redirect_to :teams, notice: "Successfully deleted!!!"
+  end
+
+  private
+  def team_params
+        params.require(:team).permit(:name)
   end
 end
