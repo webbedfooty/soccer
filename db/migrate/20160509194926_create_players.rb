ca0d293c0358640e176dc142bb1ddccc62c392f9
Patrick <patrickwiechert@yahoo.com>
2016-05-11 12:15:51 -0500

    updated model

--- db/migrate/20160509194926_create_players.rb
+++ db/migrate/20160509194926_create_players.rb
@@ -1,8 +1,8 @@
 class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
-      t.string :name
-      t.string :email
+      t.string :name, null: false
+      t.string :email, null: false
       t.timestamps null: false
     end
   end
