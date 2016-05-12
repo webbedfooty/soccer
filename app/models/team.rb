2f91b720a576b42ea1c5f8b0de7f8c04eee6ed3f
Patrick <patrickwiechert@yahoo.com>
2016-05-12 11:10:28 -0500

    added in League model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,6 @@
 class Team < ActiveRecord::Base
   validates :name, presence: true, uniqueness: {case_sensitive: false}
+
+  has_many :players
+  belongs_to :league
 end
--- db/migrate/20160509194926_create_players.rb
+++ db/migrate/20160509194926_create_players.rb
@@ -3,6 +3,7 @@ class CreatePlayers < ActiveRecord::Migration
     create_table :players do |t|
       t.string :name, null: false
       t.string :email, null: false
+
       t.timestamps null: false
     end
   end
